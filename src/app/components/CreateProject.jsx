d036e15c7017dc8d27d087559e079a69323df2ea
Haekal Dinova <dinovasala3@gmail.com>
2023-05-20 21:10:41 +0700

    message

--- src/app/components/CreateProject.jsx
+++ src/app/components/CreateProject.jsx
@@ -1,10 +1,11 @@
 'use clients';
 
-import React, { useEffect, useRef, useCallback } from 'react';
+import React, { useEffect, useRef, useCallback, useState } from 'react';
 import s from '../styles/createProject.module.css';
 import { FaTag, FaBack, FaBook, FaUser } from 'react-icons/fa';
 export default function CreateProject({ setActive, active }) {
   const uploadRef = useRef();
+  const [Tag, setTag] = useState("");
   const bubble = useCallback((e) => {
     if (uploadRef.current.contains(e.target)) return;
     setActive(false);
@@ -39,26 +40,17 @@ export default function CreateProject({ setActive, active }) {
         <label className={s['label']} for="">
           Add Tag
         </label>
-        <div className={s['input-wrapper']}>
-          <FaTag className={s['icon']} />
-          <input
-            type="text"
-            placeholder="Enter Tag name"
-            className={`${s['input']} ${s['input-tag']}`}
-          />
-          <div className={s['button']}>Add</div>
+        <div className="radio" >
+          <div><input type="radio" id="age1" name="age" value="30" onClick={() => setTag("MACH")} />
+          <label for="age1"><b>MACH</b></label></div>
+          <div><input type="radio" id="age2" name="age" value="60" onClick={() => setTag("OBSCURA")}/>
+          <label for="age2"><b>OBSCURA</b></label> </div>
+          <div><input type="radio" id="age3" name="age" value="100" onClick={() => setTag("YANG LAINNYA")}/>
+          <label for="age3"><b>YANG LAINNYA</b></label></div>
+          
+          
         </div>
-        <div className={s['box']}>
-          <div className={s['tags']}>
-            <div className={s['tag']}>James Bond</div>
-            <div className={s['tag']}>nIGGER</div>
-            <div className={s['tag']}>James Bond bARCO</div>
-            <div className={s['tag']}>es teh</div>
-            <div className={s['tag']}>bb</div>
-            <div className={s['tag']}>James Bond niGEER</div>
-            <div className={s['tag']}>James Bond bARCO</div>
-          </div>
-        </div>
+
         <label className={s['label']} for="">
           Capacity
         </label>
